90eb54fd2d924060722de113ea21c5758792aff3
Juan Sebastian Gamboa B <jgamboabotero@gmail.com>
2020-09-27 17:27:28 -0600

    Player inicio

--- main/RPGStore/src/app/Player.java
+++ main/RPGStore/src/app/Player.java
@@ -3,22 +3,35 @@ package app;
 public class Player {
     //Atributes
     private String name;
-    private int money;
+    //private int money;
     private int[] stats;
     private int[] inventory;
 
     //Constructor
     public Player(){
         name = "Diego";
-        money = 2500;
+        //money = 2500;
         for (int i = 0; i <= 5; i++){
             stats[i] = 20;
         }
     }
 
     //Methods
-    public String printName(){
+    public void printName(){
         System.out.println(name);
-        return name;
-    }    
+    }
+
+    public void printInv(){
+        for (int i = 0; i < inventory.length; i++){
+            System.out.println(inventory[i]);
+        }
+    }
+
+    public void printStats(){
+        for (int i = 0; i <=5; i++){
+            System.out.println(stats[i]);
+        }
+    }
+
+
 }
