3ebbaf05abf655fabced3275bc60c8e5a3e230e9
Juan Sebastian Gamboa B <jgamboabotero@gmail.com>
2020-09-27 17:17:15 -0600

    Update Player.java

--- main/RPGStore/src/app/Player.java
+++ main/RPGStore/src/app/Player.java
@@ -1,5 +1,24 @@
 package app;
 
 public class Player {
-    
+    //Atributes
+    private String name;
+    private int money;
+    private int[] stats;
+    private int[] inventory;
+
+    //Constructor
+    public Player(){
+        name = "Diego";
+        money = 2500;
+        for (int i = 0; i <= 5; i++){
+            stats[i] = 20;
+        }
+    }
+
+    //Methods
+    public String printName(){
+        System.out.println(name);
+        return name;
+    }    
 }
