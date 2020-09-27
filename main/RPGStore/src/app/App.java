c30766397e821542a4f44c505bb00885f4cf9a8b
Orantoon <davidsuarez2001@gmail.com>
2020-09-27 17:15:55 -0600

    Window

--- main/RPGStore/src/app/App.java
+++ main/RPGStore/src/app/App.java
@@ -1,7 +1,11 @@
 package app;
 
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Window pantalla = new Window();
+        pantalla.setVisible(true);
+        pantalla.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
--- main/RPGStore/src/app/Window.java
+++ main/RPGStore/src/app/Window.java
@@ -1,5 +1,18 @@
 package app;
 
-public class Window {
-    
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private final int Width=1600, Height=1000;
+    public Window() {
+        setTitle ("RPG Store");
+        setSize (Width, Height);
+        setLocationRelativeTo (null);
+        setResizable (false);
+    }
 }
